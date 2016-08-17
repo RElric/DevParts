a36cd0fab57d47b9a4369fa46be9587cf29cfbc2
Elric Reffuveille <contact@reffuveille.fr>
2016-08-17 11:23:00 +0200

    GIT DE SAUVEGARDE

--- twitter-card.php
+++ twitter-card.php
@@ -17,14 +17,14 @@
  * *************************************************************************************************************** */
 
 /* *************************************************************************************************************** *
- * ******************************************* @FUNCTION get_summary_card() ***************************************** *
+ * ******************************************* @FUNCTION get_summary_card() ************************************** *
  * *************************************************************************************************************** *
  * *                                                                                                             * *
- * *											 Create HTML parts wich correspond to simple Summary Twitter Card											 * *
+ * *                           Create HTML parts wich correspond to Summary Twitter Card                         * *
  * *                                                                                                             * *
  * *************************************************************************************************************** *
  * *                                                                                                             * *
- * *											  @RETURN string - A string formated in HTML to display the card									  	 * *
+ * *                        @RETURN string - A string formated in HTML to display the card                       * *
  * *                                                                                                             * *
  * ************************************************* @PARAMETERS ************************************************* *
  * *                                                                                                             * *
@@ -34,6 +34,7 @@
  * * REQUIRED      @description     - The desciption of the relative site of the card                            * *
  * * REQUIRED      @image_url       - The image url of the relative site of the card                             * *
  * * REQUIRED      @image_alt       - The alternative texte of the relative site of the card                     * *
+ * * REQUIRED      @author          - The author of the relative site of the card                                * *
  * *                                                                                                             * *
  * *************************************************************************************************************** *
  * *************************************************************************************************************** *
@@ -44,65 +45,139 @@ function get_summary_card(
     $title,
     $description,
     $image_url,
-    $image_alt
+    $image_alt,
+    $author
 ) {
     return
-        '<meta name="twitter:card" content="'.$type.'" />'.
-        '<meta name="twitter:site" content="'.$site_url.'" />'.
-        '<meta name="twitter:title" content="'.$title.'" />'.
-        '<meta name="twitter:description" content="'.$description.'" />'.
-        '<meta name="twitter:image" content="'.$image_url.'" />'.
-        '<meta name="twitter:image:alt" content="'.image_alt.'" />'
+      '<meta name="twitter:card" content="'.$type.'" />'.
+      '<meta name="twitter:site" content="'.$site_url.'" />'.
+      '<meta name="twitter:title" content="'.$title.'" />'.
+      '<meta name="twitter:description" content="'.$description.'" />'.
+      '<meta name="twitter:image" content="'.$image_url.'" />'.
+      '<meta name="twitter:creator" content="'.$author.'">'
+      '<meta name="twitter:image:alt" content="'.image_alt.'" />'
     ;
 }
 
-function get_summary_large_image_card(
+
+/* *************************************************************************************************************** *
+ * ********************************************* @FUNCTION get_app_card() **************************************** *
+ * *************************************************************************************************************** *
+ * *                                                                                                             * *
+ * *                         Create HTML parts wich correspond to Application Twitter Card                       * *
+ * *                                                                                                             * *
+ * *************************************************************************************************************** *
+ * *                                                                                                             * *
+ * *                        @RETURN string - A string formated in HTML to display the card                       * *
+ * *                                                                                                             * *
+ * ************************************************* @PARAMETERS ************************************************* *
+ * *                                                                                                             * *
+ * * REQUIRED      @type            - The type of the card                                                       * *
+ * * REQUIRED      @site_url        - The url of the relative application's site of the card                     * *
+ * * REQUIRED      @title           - The title of the relative application's site of the card                   * *
+ * * REQUIRED      @description     - The desciption of the relative application's site of the card              * *
+ * * REQUIRED      @image_url       - The image url of the relative application's site of the card               * *
+ * * REQUIRED      @image_alt       - The alternative texte of the relative application's site of the card       * *
+ * * REQUIRED      @author          - The author of the relative application's site of the card                  * *
+ * * REQUIRED      @country         - The country where the relative application is provided                     * *
+ * * REQUIRED      @app_name_*      - The app name for relative platform                                         * *
+ * * REQUIRED      @app_id_*        - The app id for relative platform         								                   * *
+ * * REQUIRED      @app_url_*       - The app url for relative platform         								                 * *
+ * *                                                                                                             * *
+ * *************************************************************************************************************** *
+ * *************************************************************************************************************** *
+ * *************************************************************************************************************** */
+function get_app_card(
     $type,
     $site_url,
     $title,
     $description,
     $image_url,
-    $image_alt
+    $image_alt,
+    $author,
+    $country,
+    $app_name_iphone,
+    $app_id_iphone,
+    $app_url_iphone,
+    $app_name_ipad,
+    $app_id_ipad,
+    $app_url_ipad,
+    $app_name_googleplay,
+    $app_id_googleplay,
+    $app_url_googleplay,
 ) {
-	$card = get_summary_card($type, $site_url, $title, $description, $image_url, $image_alt);
+  $card =  get_summary_card($type, $site_url, $title, $description, $image_url, $image_alt, $author);
 
-	/*AJOUTER LE RESTE DE LA CARTE*/
+  $card .= 
+    '<meta name="twitter:app:country" content="'.$country.'">'.
+    '<meta name="twitter:app:name:iphone" content="'.$app_name_iphone.'">'.
+    '<meta name="twitter:app:id:iphone" content="'.$app_id_iphone.'">'.
+    '<meta name="twitter:app:url:iphone" content="'.$app_url_iphone.'">'.
+    '<meta name="twitter:app:name:ipad" content="'.$app_name_ipad.'">'.
+    '<meta name="twitter:app:id:ipad" content="'.$app_id_ipad.'">'.
+    '<meta name="twitter:app:url:ipad" content="'.$app_url_ipad.'">'.
+    '<meta name="twitter:app:name:googleplay" content="'.$app_name_googleplay.'">'.
+    '<meta name="twitter:app:id:googleplay" content="'.$app_id_googleplay.'">'.
+    '<meta name="twitter:app:url:googleplay" content="'.$app_url_googleplay.'">'
+  ;
 
-	return $card;
+  return $card;
 }
 
-<!-- Summary Card with large image -->
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="@nytimes">
-<meta name="twitter:creator" content="@SarahMaslinNir">
-<meta name="twitter:title" content="Parade of Fans for Houston’s Funeral">
-<meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here.">
-<meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg">
-<meta name="twitter:image:alt" content="BLABLA_ALTERNATIF" />
 
-<!-- App Card -->
-<meta name="twitter:card" content="app">
-<meta name="twitter:site" content="@TwitterDev">
-<meta name="twitter:description" content="Cannonball is the fun way to create and share stories and poems on your phone. Start with a beautiful image from the gallery, then choose words to complete the story and share it with friends.">
-<meta name="twitter:app:country" content="US">
-<meta name="twitter:app:name:iphone" content="Cannonball">
-<meta name="twitter:app:id:iphone" content="929750075">
-<meta name="twitter:app:url:iphone" content="cannonball://poem/5149e249222f9e600a7540ef">
-<meta name="twitter:app:name:ipad" content="Cannonball">
-<meta name="twitter:app:id:ipad" content="929750075">
-<meta name="twitter:app:url:ipad" content="cannonball://poem/5149e249222f9e600a7540ef">
-<meta name="twitter:app:name:googleplay" content="Cannonball">
-<meta name="twitter:app:id:googleplay" content="io.fabric.samples.cannonball">
-<meta name="twitter:app:url:googleplay" content="http://cannonball.fabric.io/poem/5149e249222f9e600a7540ef">
+/* *************************************************************************************************************** *
+ * ***************************************** @FUNCTION get_media_player_card() *********************************** *
+ * *************************************************************************************************************** *
+ * *                                                                                                             * *
+ * *                        Create HTML parts wich correspond to media player Twitter Card                       * *
+ * *                                                                                                             * *
+ * *************************************************************************************************************** *
+ * *                                                                                                             * *
+ * *                        @RETURN string - A string formated in HTML to display the card                       * *
+ * *                                                                                                             * *
+ * ************************************************* @PARAMETERS ************************************************* *
+ * *                                                                                                             * *
+ * * REQUIRED      @type            - The type of the card                                                       * *
+ * * REQUIRED      @site_url        - The url of the relative site of the card                                   * *
+ * * REQUIRED      @title           - The title of the relative site of the card                                 * *
+ * * REQUIRED      @description     - The desciption of the relative site of the card                            * *
+ * * REQUIRED      @image_url       - The image url of the relative site of the card                             * *
+ * * REQUIRED      @image_alt       - The alternative texte of the relative site of the card                     * *
+ * * REQUIRED      @author          - The author of the relative site of the card                                * *
+ * * REQUIRED      @player_url      - The url of player of the related media of the site of the card             * *
+ * * REQUIRED      @player_width    - The width of player of the related media of the site of the card           * *
+ * * REQUIRED      @player_height   - The height of player of the related media of the site of the card          * *
+ * * REQUIRED      @player_stream_url																																						 * *
+ * *				- The stream url of player of the related media of the site of the card                    	 				 * *
+ * * REQUIRED      @player_stream_content_type          																												 * *
+ * *				- The stream content type (accorded to MIME) of player of the related media of the site of the card  * *
+ * *                                                                                                             * *
+ * *************************************************************************************************************** *
+ * *************************************************************************************************************** *
+ * *************************************************************************************************************** */
+function get_media_player_card(
+    $type,
+    $site_url,
+    $title,
+    $description,
+    $image_url,
+    $image_alt,
+    $author,
+    $player_url,
+    $player_width,
+    $player_height,
+    $player_stream_url,
+    $player_stream_content_type
+) {
+  $card =  get_summary_card($type, $site_url, $title, $description, $image_url, $image_alt, $author);
 
-<!-- Player Card -->
-	<meta name="twitter:card" content="player">
-	<meta name="twitter:site" content="@rchoi">
-	<meta name="twitter:title" content="Sample Player Card">
-	<meta name="twitter:description" content="This is a sample video. When you implement, make sure all links are secure.">
-	<meta name="twitter:image" content="https://yoursite.com/example.png">
-	<meta name="twitter:player" content="https://yoursite.com/container.html">
-	<meta name="twitter:player:width" content="480">
-	<meta name="twitter:player:height" content="480">
-	<meta name="twitter:player:stream" content="https://yoursite.com/example.mp4">
-	<meta name="twitter:player:stream:content_type" content="video/mp4">
+  $card .= 
+    '<meta name="twitter:player" content="'.$player_url.'">'.
+    '<meta name="twitter:player:width" content="'.$player_width.'">'.
+    '<meta name="twitter:player:height" content="'.$player_height.'">'.
+    '<meta name="twitter:player:stream" content="'.$player_stream_url.'">'.
+    '<meta name="twitter:player:stream:content_type" content="'.$player_stream_content_type'">'
+  ;
+
+  return $card;
+}
